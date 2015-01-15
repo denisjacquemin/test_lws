2258dc25e42edb5fc093fbc6d560efa76965579e
Denis Jacquemin <denis.jacquemin@lu.pwc.com>
2015-01-15 15:16:44 +0100

    host updated to localhost

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 
 var server = app.listen(8080, function () {
 
-  var host = server.address().address
+  var host = 'http://localhost'
   var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
